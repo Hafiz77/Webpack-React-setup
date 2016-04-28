78c754d20df950f66e976ed64019b218ddac32ca
Abdullah Al Hafiz <abdullahalhafiz77@gmail.com>
2016-04-28 11:30:23 +0600

    App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Hello World!!! Welcome to React Redux Setup...</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam nisi, explicabo, ea nesciunt id a, sint, aliquid consectetur sit quia excepturi. Repellat quo iure culpa ipsum nesciunt harum quibusdam! Maxime!</p>
+         </div>
+      );
+   }
+}
+
+export default App;
